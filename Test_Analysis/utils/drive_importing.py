7e6f2dee2dcd49792b5c81fc6d08d401d3809096
Andras <agalambos3@gmail.com>
2023-02-16 21:02:10 -0600

    file selection from finder in drive_importing

--- Test_Analysis/utils/drive_importing.py
+++ Test_Analysis/utils/drive_importing.py
@@ -1,3 +1,12 @@
+import tkinter as tk
+
+from tkinter.filedialog import askopenfilename
+tk.Tk().withdraw() # part of the import if you are not using other tkinter functions
+
+def find_file() -> str: 
+    fn = askopenfilename()
+    return fn 
+
 #Put the path of the drive directory on your device in here
 drive_dir = "/Users/andras/Library/CloudStorage/GoogleDrive-galambosa@carleton.edu/Shared drives/Research"
 
