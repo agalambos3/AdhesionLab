2a3eccd626b3b025720e3d44fceeb5eac7388b25
Andras <agalambos3@gmail.com>
2023-02-23 15:29:03 -0600

    figuring out how to make tkinter buttons do stuff

--- Test_Analysis/analysisGUI.py
+++ Test_Analysis/analysisGUI.py
@@ -9,17 +9,44 @@ import cv2 as cv
 # file = dimport.find_file()
 file= dimport.drive_import("/Code/Test Analysis/Test Analysis/Intensity Averaging Method/test_vid_trimmed.mp4")
 va = dc.video_analysis(file,10,crop=True)
-vid = va.get_video()
 
-vid.set(cv.CAP_PROP_POS_FRAMES,5000)
+def set_vframe():
+    vid = va.get_video()
+    vid.set(cv.CAP_PROP_POS_FRAMES,currentframe.get())
+    frame = vid.read()[1]
+    scale_percent = 70 # percent of original size
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
+    dim = (width, height)
+# resize image
+    img = cv.resize(frame, dim, interpolation = cv.INTER_AREA)
+    _, im_arr = cv.imencode('.png', img)  # im_arr: image in Numpy one-dim array format.
+    im_bytes = im_arr.tobytes()
+    tkimg1 = tk.PhotoImage(data=im_bytes)
+    flabel= tk.Label(image= tkimg1,background="gold",foreground="black")
+    flabel.grid(row=0,column=1)
+    print("vframe set")
 
-frame = vid.read()[1]
+def setframenumber():
+    frame= int(entry.get())
+    if type(frame) == int:
+        currentframe.set(frame)
+        print(currentframe.get())      
+    numlabel = tk.Label(text=currentframe.get(),background="white",foreground="black")
+    numlabel.grid(row=0,column=0)
+
+    
+
+
+
 
 
 
 
 
 window = tk.Tk()
+currentframe = tk.IntVar(value=5000)
+
 
 # img = Image.open(file)
 # print(img.size[0])
@@ -32,23 +59,14 @@ window = tk.Tk()
 # img = img.resize(size=(height,width))
 # img = ImageTk.PhotoImage(img)
 
-scale_percent = 70 # percent of original size
-width = int(frame.shape[1] * scale_percent / 100)
-height = int(frame.shape[0] * scale_percent / 100)
-dim = (width, height)
-# resize image
-img = cv.resize(frame, dim, interpolation = cv.INTER_AREA)
 
-_, im_arr = cv.imencode('.png', img)  # im_arr: image in Numpy one-dim array format.
-im_bytes = im_arr.tobytes()
-tkimg1 = tk.PhotoImage(data=im_bytes)
-
-
-
-flabel= tk.Label(image= tkimg1,background="gold",foreground="black")
+set_vframe()
 rlabel = tk.Label(text = "blah blah",background="blue",foreground="white")
-
-flabel.grid(row=0,column=1)
+entry = tk.Entry()
+enterbutton = tk.Button(text="Enter",command=setframenumber)
 rlabel.grid(row=1,column=1)
+entry.grid(row=0,column=3)
+enterbutton.grid(row=0,column=4)
+
 
 window.mainloop()
