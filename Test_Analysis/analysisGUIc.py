0275e5ed9a7c62b9851630f07fbc2a9ba4a40713
Andras <agalambos3@gmail.com>
2023-02-23 16:20:44 -0600

    commenting out functionality of analysisGUI class

--- Test_Analysis/analysisGUIc.py
+++ Test_Analysis/analysisGUIc.py
@@ -5,13 +5,14 @@ class analysisGUI:
     def __init__(self,root):
         self.root = root
 
-    
-    #function to set video to current frame...
-    #maybe var with 
+    #function to set video to a specific frame...
+        #maybe need tk var to track current video frame 
 
-    #function to advance frame
+    #function to advance by one frame when button pressed
+    #function to go back by one frame when button pressed
+    #function to analyze video when button pressed - include calibration where user adjusts until correct
+    #Function to go to specific frame (either by feature or by frame number idk yet)
 
 
-
-if __name__ == "main":
-    gui = analysisGUI
+if __name__ == '__main__':
+    gui = analysisGUI(tk.Tk())
